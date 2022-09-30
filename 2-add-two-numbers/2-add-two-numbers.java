12fe3865badf64c4dfe05d01fb438fbf72131656
nagswarnaa <36414037+nagswarnaa@users.noreply.github.com>
2022-09-30 16:43:19 -0500

    Time: 4 ms (45.19%), Space: 48.3 MB (19.52%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -1,60 +1,34 @@
-
- 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-           if(l1==null && l2==null){
-               return null;
-           }
-           if(l1==null){
-            return l2;
-           }
-           else if(l2==null){
-            return l1;
-           }
-           ListNode temp1=l1;
-           ListNode temp2=l2;
-           ListNode head=null;
-           ListNode temp3=null;
-           int carry=0;
-           while(temp1!=null && temp2!=null){
-               int i=temp1.val+temp2.val+carry;               
-               int add=i%10;  
-               if(head==null){
-                   head=new ListNode(add);
-                   temp3=head;
-               }
-               else{
-                   temp3.next=new ListNode(add);
-                   temp3=temp3.next;
-               }
-               carry=i/10;
-               temp1=temp1.next;
-               temp2=temp2.next;
-           }
-           if(temp1==null&& temp2!=null){
-              while(temp2!=null){
-              int i=temp2.val+carry;    
-              int add=i%10;    
-              temp3.next=new ListNode(add);
-              carry=i/10;    
-              temp3=temp3.next;
-              temp2=temp2.next;  
-              } 
-             
-           }
-           if(temp2==null&& temp1!=null){
-               while(temp1!=null){
-              int i=temp1.val+carry;    
-              int add=i%10;      
-              temp3.next=new ListNode(add);
-              carry=i/10;     
-              temp3=temp3.next;
-              temp1=temp1.next;
-               }
-           }
-         if(carry!=0){
-             temp3.next=new ListNode(carry);
-         }
+        ListNode temp1=l1,temp2=l2,temp3=null,head=null;
+        int add=0,carry=0;
+        while(temp1!=null || temp2!=null || carry!=0){
+            int a=(temp1==null?0:temp1.val);
+            int b=(temp2==null?0:temp2.val);
+             add=a+b+carry;
+            int num1=add%10;
+            if(head==null){
+                head=new ListNode(num1);
+                temp3=head;
+            }
+            else{
+                temp3.next=new ListNode(num1);
+                temp3=temp3.next;
+            }
+            carry=add/10;
+            temp1=(temp1==null?null:temp1.next);
+            temp2=(temp2==null?null:temp2.next);
+        }
         return head;
     }
 }
