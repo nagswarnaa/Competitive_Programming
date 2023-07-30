94aadda32f39175b98dc38fea30030e3e82b86b7
NAGARAJU SWARNA <36414037+nagswarnaa@users.noreply.github.com>
2023-07-30 14:18:40 -0500

    Time: 2 ms (94.2%), Space: 40.9 MB (91.72%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,30 +1,33 @@
-import java.util.*;
-
 class Solution {
     Set<Integer> mem;
     Set<String> wordDictSet;
-
     public boolean wordBreak(String s, List<String> wordDict) {
-        int len = s.length();
-        mem = new HashSet<Integer>();
-        wordDictSet = new HashSet<String>(wordDict);
-        return findWord(len, s);
+        int len=s.length();
+        mem=new HashSet<Integer>();
+        wordDictSet=new HashSet<String>(wordDict);
+        return findWord(len,s,wordDictSet);
     }
-
-    public boolean findWord(int start, String s){
-        if(start <= 0){
+    public boolean findWord(int start,String s, Set<String> wordDictSet){
+        if(start<=0){
             return true;
         }
         if(mem.contains(start)){
             return false;
         }
-        for(String x : wordDictSet){
-            int lenSub = x.length();
-            if(start - lenSub >= 0 && x.equals(s.substring(start - lenSub, start)) && findWord(start - lenSub, s)){
-                return true;
-            } 
+        for(String x:wordDictSet){
+               int lenSub=x.length();
+               if(start-lenSub>=0 && x.equals(s.substring(start-lenSub,start)) && findWord(start-lenSub,s,wordDictSet)){
+                  
+                  return true;
+               } 
         }
-        mem.add(start);
+        mem.add(start); 
         return false;
+    }
+    public boolean compare(int start,int len, String source,String sub){
+        if(start-len<0){
+            return false;
+        }
+        return sub.equals(source.substring(start-len,start));
     }
 }
