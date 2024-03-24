75d039d6f550757ffd2706a6d7d260f5c8dab603
NAGARAJU SWARNA <36414037+nagswarnaa@users.noreply.github.com>
2024-03-24 17:27:39 -0500

    Time: 772 ms (11.72%), Space: 30.7 MB (63.9%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -2,37 +2,21 @@ class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         n=len(heights)
         left=[0]*n
-        right=[0]*n
+        right=[n-1]*n
         stk=[]
         #left bounds
         for x in range(n):
-            if stk==[]:
-                stk.append(x)
-                left[x]=x
-            else:
-                if heights[x]<=heights[stk[-1]]:
-                    while stk!=[] and heights[x]<=heights[stk[-1]]:
-                        stk.pop()
-                if stk==[]:
-                    left[x]=0
-                else:
-                    left[x]=stk[-1]+1 
-                stk.append(x)
+            while stk and heights[x]<=heights[stk[-1]]:
+                stk.pop()
+            left[x] = stk[-1]+1 if stk else 0
+            stk.append(x)
         stk=[]        
         # right bounds
         for x in range(n-1,-1,-1):
-            if stk==[]:
-                stk.append(x)
-                right[x]=n-1
-            else:
-                if heights[x]<=heights[stk[-1]]:
-                    while stk!=[] and heights[x]<=heights[stk[-1]]:
-                        stk.pop()
-                if stk==[]:
-                    right[x]=n-1
-                else:
-                    right[x]=stk[-1]-1 
-                stk.append(x) 
+            while stk!=[] and heights[x]<=heights[stk[-1]]:
+                stk.pop()
+            right[x] = stk[-1]-1 if stk else n-1
+            stk.append(x) 
         max_area=0
         for i in range(n):
             max_area=max(max_area,((right[i]-left[i]+1)*heights[i]))
